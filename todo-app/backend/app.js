6aecc0387f5b5ef0faab62db298ca463f9135036
Kushagra Agarwal <kushagra3204@gmail.com>
2024-10-21 00:21:51 +0530

    Task Updated

--- todo-app/backend/app.js
+++ todo-app/backend/app.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: 'https://kaushalam-todo-task.vercel.app',
+    origin: 'https://kaushalam-todo-task.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
