294426e7698cd3d11dd2f5ee5c42cc9727cbea34
Kushagra Agarwal <kushagra3204@gmail.com>
2024-10-21 00:16:55 +0530

    Task Updated

--- todo-app/backend/app.js
+++ todo-app/backend/app.js
@@ -1,21 +1,24 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
-import user from './routes/user.js'
-import todo from './routes/todo.js'
-
-app.use('/user', user)
-app.use('/todo', todo)
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+const app = express();
 
 app.use(cors({
-    origin: 'https://kaushalam-todo-task.vercel.app/',
-}))
+    origin: 'https://kaushalam-todo-task.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
 
-export default app
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+import user from './routes/user.js';
+import todo from './routes/todo.js';
+
+app.use('/user', user);
+app.use('/todo', todo);
+
+export default app;
